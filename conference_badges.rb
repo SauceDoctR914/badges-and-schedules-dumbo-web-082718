57a59d083006ff7afc4827c22377c83734dad24a
Gavin Soko <sokolofg@gmail.com>
2018-08-14 03:09:26 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,12 @@
   def badge_maker(name)
        "Hello, my name is #{name}."
   end
- def batch_badge_creator
+  
+ def batch_badge_creator(array)
+   array.each do |name| 
+       "Hello, my name is #{name}"
+  end
+end
 
   
   def assign_rooms(list)
